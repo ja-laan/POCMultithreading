a3be96a0e920964347e05e20fa70474a1cf07edb
Joost Laan <j.laan@student.fontys.nl>
2020-04-11 18:18:38 +0200

    Add assignment threading the counter

---
+++ ThreadingTheCounters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingTheCounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{
+			//Start 5 threads			
+			for( int i = 0; i < 6; i++)	new Thread(DoThread).Start();
+
+			//Do not close the console immidiatly
+			Console.ReadLine();
+		}
+
+		public static void DoThread()
+		{
+			for (int i = 0; i < 101; i++)
+			{
+				Console.Write(i + " ");
+			}
+		}
+	}
+}
